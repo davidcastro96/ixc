b2018d0f379cbf0b5722fa68c967d893503e6e52
davidcastro96 <davidcastro0796@outlook.es>
2022-03-17 00:05:42 -0500

    crear usuarios

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const request = require("request");
 const exphbs = require("express-handlebars");
-const bp = require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 
+const url =
+  "https://ICXCandidate:Welcome2021@imaginecx--tst2.custhelp.com/services/rest/connect/v1.3/contacts";
+/* ----------------------------------------------- */
 //inicio el motor de plantillas
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
-
+/* ----------------------------------------------- */
 //método para analizar las solicitudes entrantes
-app.use(bp.json());
-app.use(bp.urlencoded({ extended: true }));
-
+app.use(bodyParser.json()); // body en formato json
+app.use(bodyParser.urlencoded({ extended: true }));
+/* ----------------------------------------------- */
+//carpeta de estaticos
+app.use(express.static("public"));
+/* ----------------------------------------------- */
 //ruta
 app.get("/", (req, res) => {
   request(
     //ruta de la API de IXC
-    "http://ICXCandidate:Welcome2021@imaginecx--tst2.custhelp.com/services/rest/connect/v1.3/contacts",
+    url,
     (err, response, body) => {
       if (!err) {
         const contacts = JSON.parse(body);
@@ -29,16 +35,30 @@ app.get("/", (req, res) => {
     }
   );
 });
-
-app.get("/crear", (req, res) => {
-  res.render("crear", {
-    layout: "main",
+/* ----------------------------------------------- */
+app.get("/form", (req, res) => {
+  res.render("form", { layout: "main" });
+});
+/* ----------------------------------------------- */
+app.post("/form", (req, res) => {
+  const form = req.body;
+  const options = {
+    uri: url,
+    method: "POST",
+    json: {
+      login: form.login,
+      name: {
+        first: form.nombre,
+        last: form.apellido,
+      },
+    },
+  };
+  request(options, (error, response, body) => {
+    if (!error) {
+      res.render("form", { layout: "main", form: form });
+    }
   });
 });
-
-app.post("/form", (req, res) => {
-  res.send(req.body);
-});
-
+/* ----------------------------------------------- */
 //Inicia el server
 app.listen(3000);
